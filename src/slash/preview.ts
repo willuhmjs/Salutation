2ac0406ec12c8c310ff129cf456345689c754d9a
Will Faircloth <willfaircloth@icloud.com>
2022-11-08 14:21:31 -0500

    add preview user

--- src/slash/preview.ts
+++ src/slash/preview.ts
@@ -5,7 +5,8 @@ import type { CommandLike } from "./command";
 export default <CommandLike>{
 	data: new SlashCommandBuilder()
 		.setName("preview")
-		.setDescription("Preview your welcome image (if you have one)."),
+		.setDescription("Preview your welcome image (if you have one).")
+		.addUserOption((option) => option.setName("user").setDescription("The user to preview the welcome image for.").setRequired(false)),
 	async execute(client, interaction) {
 		const { Server } = (client as any).Schema;
 
@@ -15,7 +16,7 @@ export default <CommandLike>{
 			await interaction.reply({
 				embeds: [new EmbedBuilder().setDescription("Loading example welcome message...").setColor("Blurple")]
 			});
-			let attachment = await createWelcome(interaction.user, interaction.guild);
+			let attachment = await createWelcome(interaction.options.getUser("user") ?? interaction.user, interaction.guild);
 			await interaction.editReply({
 				embeds: [new EmbedBuilder().setDescription("Preview of your welcome image:").setImage("attachment://welcome.jpg").setColor("Green")],
 				files: [{ attachment, name: "welcome.jpg" }],
