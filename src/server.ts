2bfb33166bc99f8e3c3e8e2c9a6b3aa95794f924
Will Faircloth <willfaircloth@icloud.com>
2022-11-07 21:19:21 -0500

    format

--- src/server.ts
+++ src/server.ts
@@ -3,52 +3,54 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 // Create the server schema
-const serverSchema = new Schema({
- id: {
-   required: true,
-   type: String
- },
- background: {
-   required: true,
-   type: String,
-   default: "#000000"
- },
- font: {
-   required: true,
-   type: String,
-   default: "whitney"
- },
- font_color: {
-   required: true,
-   type: String,
-   default: "#FFFFFF"
- },
- channel: {
-   required: true,
-   type: String
- },
- message: {
-   required: true,
-   type: String,
-   default: "Welcome to the server"
- },
- memberCount: {
-   required: true,
-   type: Boolean,
-   default: false
- },
- ping: {
-   required: true,
-   type: Boolean,
-   default: true
- },
- image: {
-   required: true,
-   type: Boolean,
-   default: false
- }
-
-}, {timestamps: true});
+const serverSchema = new Schema(
+	{
+		id: {
+			required: true,
+			type: String,
+		},
+		background: {
+			required: true,
+			type: String,
+			default: "#000000",
+		},
+		font: {
+			required: true,
+			type: String,
+			default: "whitney",
+		},
+		font_color: {
+			required: true,
+			type: String,
+			default: "#FFFFFF",
+		},
+		channel: {
+			required: true,
+			type: String,
+		},
+		message: {
+			required: true,
+			type: String,
+			default: "Welcome to the server",
+		},
+		memberCount: {
+			required: true,
+			type: Boolean,
+			default: false,
+		},
+		ping: {
+			required: true,
+			type: Boolean,
+			default: true,
+		},
+		image: {
+			required: true,
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
 
 // Export model
 const Server = mongoose.model("Server", serverSchema);
--- src/slash/preview.ts
+++ src/slash/preview.ts
@@ -8,23 +8,28 @@ export default <CommandLike>{
 		.setDescription("Preview your welcome image (if you have one)."),
 	async execute(client, interaction) {
 		const { Server } = (client as any).Schema;
-	
-		let [server] = await Server.find({id: interaction.guild.id});
+
+		let [server] = await Server.find({ id: interaction.guild.id });
 
 		if (server) {
 			await interaction.deferReply();
 			let attachment = await createWelcome(interaction.user, interaction.guild);
 			const embed = new EmbedBuilder()
-			.setDescription("Preview of your welcome image:")
-			.setImage("attachment://welcome.jpg");
-			await interaction.editReply({embeds: [embed], files: [{attachment, name: "welcome.jpg"}]});
-			interaction.editReply({embeds: [embed], files: [attachment]});
+				.setDescription("Preview of your welcome image:")
+				.setImage("attachment://welcome.jpg");
+			await interaction.editReply({
+				embeds: [embed],
+				files: [{ attachment, name: "welcome.jpg" }],
+			});
+			interaction.editReply({ embeds: [embed], files: [attachment] });
 		} else {
 			const errorEmbed = new EmbedBuilder()
-			.setDescription("You don't have a welcome image set up yet! Use `/setup` to set one up.")
-			.setColor("Red")
-			.setTimestamp();
-			interaction.reply({embeds: [errorEmbed]});
+				.setDescription(
+					"⚠️ You don't have a welcome image set up yet! Use `/setup` to set one up."
+				)
+				.setColor("Red")
+				.setTimestamp();
+			interaction.reply({ embeds: [errorEmbed] });
 		}
 	},
 };
